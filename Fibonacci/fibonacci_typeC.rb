2e714226b7cf5e42c919895a5cf28f852e8ead85
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-05 23:25:33 +0900

    フィボナッチのtypeCを追加

---
+++ Fibonacci/fibonacci_typeC.rb
@@ -0,0 +1,22 @@
+# 再帰的に書きつつ　[メモ化] する。
+@f = []
+def fib2(n)
+  if n <= 1
+    return 1
+  else
+    if @f[n]
+      return @f[n]
+    else
+      @f[n] = fib2(n-1) + fib2(n-2)
+    end
+  end
+end
+
+# フィボナッチ数列の生成
+print "生成する個数："
+create_time = gets.to_i
+fib2(create_time)
+
+# 出力
+require 'pp'
+pp @f
